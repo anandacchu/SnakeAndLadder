959b23ce1e31929f5247e96aa8d61cad4d10016e
anand acchu <you@example.com>
2022-08-09 06:54:23 +0530

    welcome to snakeandladder game

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    class gameone
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Snake And Ladder game");
+            Console.WriteLine("your start position is zero");
+        }
+
+    }
+}
+
