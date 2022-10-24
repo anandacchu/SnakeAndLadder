dd9413fb119803f246cafc9f65de70729d0e3e10
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-24 17:07:03 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder game");
-            Console.WriteLine("your start position is zero");
+            
         }
 
     }
