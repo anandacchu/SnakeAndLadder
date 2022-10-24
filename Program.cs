9ab6f9bf8f57a7e44904dc8c1fee8cbcfd93cd56
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-10-24 17:17:20 +0530

    welcome

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Snake And Ladder game");
+            RandomNumber.Roll_The_Die();
             
         }
 
